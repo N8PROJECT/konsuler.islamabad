c5da9047f0684d87bc86a96b6b4109c709cb3192
N8PROJECT <151629931+N8PROJECT@users.noreply.github.com>
2025-06-21 15:14:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,3 @@ use App\Http\Controllers\Auth\OtpVerificationController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/login', [AuthController::class, 'login_form'])->name('login');
-
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
-
-Route::get('/dashboard-admin', function () {
-    return view('dashboard-admin');
-})->middleware(['auth'])->name('admin.dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard-user', function () {
-        return view('dashboard-user');
-    })->name('user.dashboard');
-
-    // Hanya untuk role user (cek di controller)
-    Route::get('/applications/create', [ApplicationController::class, 'create'])->name('applications.create');
-    Route::get('/applications', [ApplicationController::class, 'index'])->name('applications.index');
-    Route::get('/applications/{id}', [ApplicationController::class, 'show'])->name('applications.show');
-    Route::patch('/applications/{application}/cancel', [ApplicationController::class, 'cancel'])->name('applications.cancel');
-    Route::post('/applications/store', [ApplicationController::class, 'store'])->name('applications.store');
-});
